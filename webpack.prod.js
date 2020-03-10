38919b79fac0a1eeb2b2bda1c30fccbb5088e3ae
danh.le <danh.danh20051995@gmail.com>
2020-03-10 23:19:27 +0700

    update webpack production config

--- webpack.prod.js
+++ webpack.prod.js
@@ -13,6 +13,7 @@ const TerserPlugin = require('terser-webpack-plugin')
 const safePostCssParser = require('postcss-safe-parser')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
 
 const paths = require('./config/paths')
 const common = require('./webpack.common.js')
@@ -49,81 +50,106 @@ module.exports = merge(firstRequire, common, {
     // This is the URL that app is served from. We use "/" in development.
     publicPath: '/',
     // Point sourcemap entries to original disk location
-    devtoolModuleFilenameTemplate: info => path
-      .relative(paths.appSrc, info.absoluteResourcePath)
-      .replace(/\\/g, '/'),
+    devtoolModuleFilenameTemplate: info => path.relative(paths.appSrc, info.absoluteResourcePath),
   },
+
+  plugins: [
+    new SWPrecacheWebpackPlugin({
+      // By default, a cache-busting query parameter is appended to requests
+      // used to populate the caches, to ensure the responses are fresh.
+      // If a URL is already hashed by Webpack, then there is no concern
+      // about it being stale, and the cache-busting can be skipped.
+      dontCacheBustUrlsMatching: /\.\w{8}\./,
+      filename: 'service-worker.js',
+      logger(message) {
+        if (message.indexOf('Total precache size is') === 0) {
+          // This message occurs for every build and is a bit too noisy.
+          return;
+        }
+        console.log(message);
+      },
+      minify: true,
+      // For unknown URLs, fallback to the index page
+      navigateFallback: paths.servedPath.slice(0, -1) + '/index.html',
+      // Ignores URLs starting from /__ (useful for Firebase):
+      // https://github.com/facebookincubator/create-react-app/issues/2237#issuecomment-302693219
+      navigateFallbackWhitelist: [/^(?!\/__).*/],
+      // Don't precache sourcemaps (they're large) and build asset manifest:
+      staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
+      // Work around Windows path issue in SWPrecacheWebpackPlugin:
+      // https://github.com/facebookincubator/create-react-app/issues/2235
+      stripPrefix: paths.appBuild.replace(/\\/g, '/') + '/',
+    })
+  ],
 
   // https://webpack.js.org/configuration/optimization
   optimization: {
     nodeEnv: 'production',
-    splitChunks: {
-      chunks: 'all',
-      name: false,
-    },
-    // minimizer: [
-    //   // This is only used in production mode
-    //   new TerserPlugin({
-    //     terserOptions: {
-    //       parse: {
-    //         // We want terser to parse ecma 8 code. However, we don't want it
-    //         // to apply any minification steps that turns valid ecma 5 code
-    //         // into invalid ecma 5 code. This is why the 'compress' and 'output'
-    //         // sections only apply transformations that are ecma 5 safe
-    //         // https://github.com/facebook/create-react-app/pull/4234
-    //         ecma: 8,
-    //       },
-    //       compress: {
-    //         ecma: 5,
-    //         warnings: false,
-    //         // Disabled because of an issue with Uglify breaking seemingly valid code:
-    //         // https://github.com/facebook/create-react-app/issues/2376
-    //         // Pending further investigation:
-    //         // https://github.com/mishoo/UglifyJS2/issues/2011
-    //         comparisons: false,
-    //         // Disabled because of an issue with Terser breaking valid code:
-    //         // https://github.com/facebook/create-react-app/issues/5250
-    //         // Pending further investigation:
-    //         // https://github.com/terser-js/terser/issues/120
-    //         inline: 2,
-    //       },
-    //       mangle: {
-    //         safari10: true,
-    //       },
-    //       // Added for profiling in devtools
-    //       keep_classnames: true,
-    //       keep_fnames: true,
-    //       output: {
-    //         ecma: 5,
-    //         comments: false,
-    //         // Turned on because emoji and regex is not minified properly using default
-    //         // https://github.com/facebook/create-react-app/issues/2488
-    //         ascii_only: true,
-    //       },
-    //     },
-    //     // Use multi-process parallel running to improve the build speed
-    //     // Default number of concurrent runs: os.cpus().length - 1
-    //     // Disabled on WSL (Windows Subsystem for Linux) due to an issue with Terser
-    //     // https://github.com/webpack-contrib/terser-webpack-plugin/issues/21
-    //     parallel: !isWsl,
-    //     // Enable file caching
-    //     cache: true,
-    //     sourceMap: true,
-    //   }),
-    //   // This is only used in production mode
-    //   new OptimizeCSSAssetsPlugin({
-    //     cssProcessorOptions: {
-    //       parser: safePostCssParser,
-    //       map: {
-    //         // `inline: false` forces the sourcemap to be output into a
-    //         // separate file
-    //         inline: false,
-    //         // `annotation: true` appends the sourceMappingURL to the end of
-    //         // the css file, helping the browser find the sourcemap
-    //         annotation: true,
-    //       },
-    //     },
-    //   }),
-    // ],
+    // Minify the code.
+    minimize: true,
+    minimizer: [
+      // This is only used in production mode
+      new TerserPlugin({
+        terserOptions: {
+          parse: {
+            // We want terser to parse ecma 8 code. However, we don't want it
+            // to apply any minification steps that turns valid ecma 5 code
+            // into invalid ecma 5 code. This is why the 'compress' and 'output'
+            // sections only apply transformations that are ecma 5 safe
+            // https://github.com/facebook/create-react-app/pull/4234
+            ecma: 8,
+          },
+          compress: {
+            ecma: 5,
+            warnings: false,
+            // Disabled because of an issue with Uglify breaking seemingly valid code:
+            // https://github.com/facebook/create-react-app/issues/2376
+            // Pending further investigation:
+            // https://github.com/mishoo/UglifyJS2/issues/2011
+            comparisons: false,
+            // Disabled because of an issue with Terser breaking valid code:
+            // https://github.com/facebook/create-react-app/issues/5250
+            // Pending further investigation:
+            // https://github.com/terser-js/terser/issues/120
+            inline: 2,
+          },
+          mangle: {
+            safari10: true,
+          },
+          // Added for profiling in devtools
+          keep_classnames: true,
+          keep_fnames: true,
+          output: {
+            ecma: 5,
+            comments: false,
+            // Turned on because emoji and regex is not minified properly using default
+            // https://github.com/facebook/create-react-app/issues/2488
+            ascii_only: true,
+          },
+        },
+        // Use multi-process parallel running to improve the build speed
+        // Default number of concurrent runs: os.cpus().length - 1
+        // Disabled on WSL (Windows Subsystem for Linux) due to an issue with Terser
+        // https://github.com/webpack-contrib/terser-webpack-plugin/issues/21
+        parallel: !isWsl,
+        // Enable file caching
+        cache: true,
+        sourceMap: true,
+      }),
+      // This is only used in production mode
+      new OptimizeCSSAssetsPlugin({
+        cssProcessorOptions: {
+          parser: safePostCssParser,
+          map: {
+            // `inline: false` forces the sourcemap to be output into a
+            // separate file
+            inline: false,
+            // `annotation: true` appends the sourceMappingURL to the end of
+            // the css file, helping the browser find the sourcemap
+            annotation: true,
+          },
+        },
+      }),
+    ],
   }
 })
