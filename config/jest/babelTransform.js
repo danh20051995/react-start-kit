baddd27790ac915fdc1b641371fb2aea60cbe1e3
danh.le <danh.danh20051995@gmail.com>
2021-03-10 21:28:11 +0700

    fix test jest transform config

--- config/jest/babelTransform.js
+++ config/jest/babelTransform.js
@@ -27,3 +27,5 @@ module.exports = babelJestGlobTransform.createTransformer({
   babelrc: false,
   configFile: false
 })
+
+module.exports.createTransformer = babelJestGlobTransform.createTransformer
